StevenJiang1110/asterinas-new

Use IoVec-based reader/writer to refactor network APIs

kernel/src/net/socket/ip/datagram/bound.rs
@@ -11,6 +11,7 @@ use crate::{
     },
     prelude::*,
     process::signal::Pollee,
+    util::{IoVecRead, IoVecWrite},
 };
 
 pub struct BoundDatagram {
@@ -38,43 +39,63 @@ impl BoundDatagram {
         self.remote_endpoint = Some(*endpoint)
     }
 
-    pub fn try_recv(&self, buf: &mut [u8], _flags: SendRecvFlags) -> Result<(usize, IpEndpoint)> {
-        let result = self
-            .bound_socket
-            .raw_with(|socket: &mut RawUdpSocket| socket.recv_slice(buf));
+    pub fn try_recv(
+        &self,
+        writer: &mut dyn IoVecWrite,
+        _flags: SendRecvFlags,
+    ) -> Result<(usize, IpEndpoint)> {
+        let result = self.bound_socket.raw_with(|socket: &mut RawUdpSocket| {
+            socket.recv().map(|(packet, udp_metadata)| {
+                let copied_res = writer.write_all(&mut VmReader::from(packet));
+                let endpoint = udp_metadata.endpoint;
+                (copied_res, endpoint)
+            })
+        });
+
         match result {
-            Ok((recv_len, udp_metadata)) => Ok((recv_len, udp_metadata.endpoint)),
+            Ok((Ok(res), endpoint)) => Ok((res, endpoint)),
+            Ok((Err(e), _)) => Err(e),
             Err(RecvError::Exhausted) => {
                 return_errno_with_message!(Errno::EAGAIN, "the receive buffer is empty")
             }
             Err(RecvError::Truncated) => {
-                todo!();
+                unreachable!("`Socket::recv` should never fail with `RecvError::Truncated`")
             }
         }
     }
 
     pub fn try_send(
         &self,
-        buf: &[u8],
+        reader: &mut dyn IoVecRead,
         remote: &IpEndpoint,
         _flags: SendRecvFlags,
     ) -> Result<usize> {
-        let result = self.bound_socket.raw_with(|socket: &mut RawUdpSocket| {
-            if socket.payload_send_capacity() < buf.len() {
-                return None;
+        let reader_len = reader.len();
+
+        self.bound_socket.raw_with(|socket: &mut RawUdpSocket| {
+            if socket.payload_send_capacity() < reader_len {
+                return_errno_with_message!(Errno::EMSGSIZE, "the message is too large");
             }
-            Some(socket.send_slice(buf, *remote))
-        });
-        match result {
-            Some(Ok(())) => Ok(buf.len()),
-            Some(Err(SendError::BufferFull)) => {
-                return_errno_with_message!(Errno::EAGAIN, "the send buffer is full")
-            }
-            Some(Err(SendError::Unaddressable)) => {
-                return_errno_with_message!(Errno::EINVAL, "the destination address is invalid")
-            }
-            None => return_errno_with_message!(Errno::EMSGSIZE, "the message is too large"),
-        }
+
+            let socket_buffer = match socket.send(reader_len, *remote) {
+                Ok(socket_buffer) => socket_buffer,
+                Err(SendError::BufferFull) => {
+                    return_errno_with_message!(Errno::EAGAIN, "the send buffer is full")
+                }
+                Err(SendError::Unaddressable) => {
+                    return_errno_with_message!(Errno::EINVAL, "the destination address is invalid")
+                }
+            };
+
+            // FIXME: If copy failed, we should not send any packet.
+            // But current smoltcp API seems not to support this behavior.
+            reader
+                .read_exact(&mut VmWriter::from(socket_buffer))
+                .map_err(|e| {
+                    warn!("unexpected UDP packet will be sent");
+                    e
+                })
+        })
     }
 
     pub(super) fn init_pollee(&self, pollee: &Pollee) {

kernel/src/net/socket/vsock/common.rs
@@ -16,7 +16,7 @@ use super::{
         listen::Listen,
     },
 };
-use crate::{events::IoEvents, prelude::*, return_errno_with_message};
+use crate::{events::IoEvents, prelude::*, return_errno_with_message, util::IoVecRead};
 
 /// Manage all active sockets
 pub struct VsockSpace {
@@ -190,10 +190,15 @@ impl VsockSpace {
     }
 
     /// Send a data packet
-    pub fn send(&self, buffer: &[u8], info: &mut ConnectionInfo) -> Result<()> {
+    pub fn send(&self, reader: &mut dyn IoVecRead, info: &mut ConnectionInfo) -> Result<()> {
+        // FIXME: Creating this buffer should be avoided
+        // if the underlying driver can accept reader.
+        let mut buffer = vec![0u8; reader.len()];
+        reader.read_exact(&mut VmWriter::from(buffer.as_mut_slice()))?;
+
         let mut driver = self.driver.disable_irq().lock();
         driver
-            .send(buffer, info)
+            .send(&buffer, info)
             .map_err(|_| Error::with_message(Errno::EIO, "cannot send data packet"))
     }
 

kernel/src/net/socket/vsock/stream/socket.rs
@@ -9,13 +9,12 @@ use crate::{
     events::IoEvents,
     fs::{file_handle::FileLike, utils::StatusFlags},
     net::socket::{
-        util::{copy_message_from_user, copy_message_to_user, create_message_buffer},
         vsock::{addr::VsockSocketAddr, VSOCK_GLOBAL},
         MessageHeader, SendRecvFlags, SockShutdownCmd, Socket, SocketAddr,
     },
     prelude::*,
     process::signal::{Pollable, Poller},
-    util::IoVec,
+    util::{IoVecRead, IoVecWrite},
 };
 
 pub struct VsockStreamSocket {
@@ -81,17 +80,21 @@ impl VsockStreamSocket {
         Ok((socket, peer_addr.into()))
     }
 
-    fn send(&self, buf: &[u8], flags: SendRecvFlags) -> Result<usize> {
+    fn send(&self, reader: &mut dyn IoVecRead, flags: SendRecvFlags) -> Result<usize> {
         let inner = self.status.read();
         match &*inner {
-            Status::Connected(connected) => connected.send(buf, flags),
+            Status::Connected(connected) => connected.send(reader, flags),
             Status::Init(_) | Status::Listen(_) => {
                 return_errno_with_message!(Errno::EINVAL, "the socket is not connected");
             }
         }
     }
 
-    fn try_recv(&self, buf: &mut [u8], _flags: SendRecvFlags) -> Result<(usize, SocketAddr)> {
+    fn try_recv(
+        &self,
+        writer: &mut dyn IoVecWrite,
+        _flags: SendRecvFlags,
+    ) -> Result<(usize, SocketAddr)> {
         let connected = match &*self.status.read() {
             Status::Connected(connected) => connected.clone(),
             Status::Init(_) | Status::Listen(_) => {
@@ -99,7 +102,7 @@ impl VsockStreamSocket {
             }
         };
 
-        let read_size = connected.try_recv(buf)?;
+        let read_size = connected.try_recv(writer)?;
         connected.update_io_events();
 
         let peer_addr = self.peer_addr()?;
@@ -113,11 +116,15 @@ impl VsockStreamSocket {
         Ok((read_size, peer_addr))
     }
 
-    fn recv(&self, buf: &mut [u8], flags: SendRecvFlags) -> Result<(usize, SocketAddr)> {
+    fn recv(
+        &self,
+        writer: &mut dyn IoVecWrite,
+        flags: SendRecvFlags,
+    ) -> Result<(usize, SocketAddr)> {
         if self.is_nonblocking() {
-            self.try_recv(buf, flags)
+            self.try_recv(writer, flags)
         } else {
-            self.wait_events(IoEvents::IN, || self.try_recv(buf, flags))
+            self.wait_events(IoEvents::IN, || self.try_recv(writer, flags))
         }
     }
 }
@@ -138,19 +145,16 @@ impl FileLike for VsockStreamSocket {
     }
 
     fn read(&self, writer: &mut VmWriter) -> Result<usize> {
-        let mut buf = vec![0u8; writer.avail()];
         // TODO: Set correct flags
         let read_len = self
-            .recv(&mut buf, SendRecvFlags::empty())
+            .recv(writer, SendRecvFlags::empty())
             .map(|(len, _)| len)?;
-        writer.write_fallible(&mut buf.as_slice().into())?;
         Ok(read_len)
     }
 
     fn write(&self, reader: &mut VmReader) -> Result<usize> {
-        let buf = reader.collect()?;
         // TODO: Set correct flags
-        self.send(&buf, SendRecvFlags::empty())
+        self.send(reader, SendRecvFlags::empty())
     }
 
     fn status_flags(&self) -> StatusFlags {
@@ -285,7 +289,7 @@ impl Socket for VsockStreamSocket {
 
     fn sendmsg(
         &self,
-        io_vecs: &[IoVec],
+        reader: &mut dyn IoVecRead,
         message_header: MessageHeader,
         flags: SendRecvFlags,
     ) -> Result<usize> {
@@ -301,28 +305,24 @@ impl Socket for VsockStreamSocket {
             warn!("sending control message is not supported");
         }
 
-        let buf = copy_message_from_user(io_vecs);
-        self.send(&buf, flags)
+        self.send(reader, flags)
     }
 
-    fn recvmsg(&self, io_vecs: &[IoVec], flags: SendRecvFlags) -> Result<(usize, MessageHeader)> {
+    fn recvmsg(
+        &self,
+        writer: &mut dyn IoVecWrite,
+        flags: SendRecvFlags,
+    ) -> Result<(usize, MessageHeader)> {
         // TODO: Deal with flags
         debug_assert!(flags.is_all_supported());
 
-        let mut buf = create_message_buffer(io_vecs);
-
-        let (received_bytes, _) = self.recv(&mut buf, flags)?;
-
-        let copied_bytes = {
-            let message = &buf[..received_bytes];
-            copy_message_to_user(io_vecs, message)
-        };
+        let (received_bytes, _) = self.recv(writer, flags)?;
 
         // TODO: Receive control message
 
         let messsge_header = MessageHeader::new(None, None);
 
-        Ok((copied_bytes, messsge_header))
+        Ok((received_bytes, messsge_header))
     }
 
     fn addr(&self) -> Result<SocketAddr> {

kernel/src/util/ring_buffer.rs
@@ -10,6 +10,7 @@ use align_ext::AlignExt;
 use inherit_methods_macro::inherit_methods;
 use ostd::mm::{FrameAllocOptions, Segment, VmIo};
 
+use super::{IoVecRead, IoVecWrite};
 use crate::prelude::*;
 
 /// A lock-free SPSC FIFO ring buffer backed by a [`Segment`].
@@ -220,13 +221,10 @@ impl<T: Pod> RingBuffer<T> {
 }
 
 impl RingBuffer<u8> {
-    /// Writes data from the `VmReader` to the `RingBuffer`.
+    /// Writes data from the `reader` to the `RingBuffer`.
     ///
     /// Returns the number of bytes written.
-    pub fn write_fallible(
-        &mut self,
-        reader: &mut VmReader,
-    ) -> core::result::Result<usize, (Error, usize)> {
+    pub fn write_fallible(&mut self, reader: &mut dyn IoVecRead) -> Result<usize> {
         let mut producer = Producer {
             rb: self,
             phantom: PhantomData,
@@ -234,13 +232,10 @@ impl RingBuffer<u8> {
         producer.write_fallible(reader)
     }
 
-    /// Reads data from the `VmWriter` to the `RingBuffer`.
+    /// Reads data from the `writer` to the `RingBuffer`.
     ///
     /// Returns the number of bytes read.
-    pub fn read_fallible(
-        &mut self,
-        writer: &mut VmWriter,
-    ) -> core::result::Result<usize, (Error, usize)> {
+    pub fn read_fallible(&mut self, writer: &mut dyn IoVecWrite) -> Result<usize> {
         let mut consumer = Consumer {
             rb: self,
             phantom: PhantomData,
@@ -310,38 +305,26 @@ impl<R: Deref<Target = RingBuffer<u8>>> Producer<u8, R> {
     /// Writes data from the `VmReader` to the `RingBuffer`.
     ///
     /// Returns the number of bytes written.
-    pub fn write_fallible(
-        &mut self,
-        reader: &mut VmReader,
-    ) -> core::result::Result<usize, (Error, usize)> {
+    pub fn write_fallible(&mut self, reader: &mut dyn IoVecRead) -> Result<usize> {
         let rb = &self.rb;
         let free_len = rb.free_len();
         if free_len == 0 {
             return Ok(0);
         }
-        let write_len = reader.remain().min(free_len);
+        let write_len = reader.len().min(free_len);
 
         let tail = rb.tail();
         let write_len = if tail + write_len > rb.capacity {
             // Write into two separate parts
             let mut writer = rb.segment.writer().skip(tail).limit(rb.capacity - tail);
-            let mut len = writer.write_fallible(reader).map_err(|(e, l1)| {
-                rb.advance_tail(tail, l1);
-                (e.into(), l1)
-            })?;
+            let mut len = reader.read_exact(&mut writer)?;
 
             let mut writer = rb.segment.writer().limit(write_len - (rb.capacity - tail));
-            len += writer.write_fallible(reader).map_err(|(e, l2)| {
-                rb.advance_tail(tail, len + l2);
-                (e.into(), len + l2)
-            })?;
+            len += reader.read_exact(&mut writer)?;
             len
         } else {
             let mut writer = rb.segment.writer().skip(tail).limit(write_len);
-            writer.write_fallible(reader).map_err(|(e, len)| {
-                rb.advance_tail(tail, len);
-                (e.into(), len)
-            })?
+            reader.read_exact(&mut writer)?
         };
 
         rb.advance_tail(tail, write_len);
@@ -418,38 +401,26 @@ impl<R: Deref<Target = RingBuffer<u8>>> Consumer<u8, R> {
     /// Reads data from the `VmWriter` to the `RingBuffer`.
     ///
     /// Returns the number of bytes read.
-    pub fn read_fallible(
-        &mut self,
-        writer: &mut VmWriter,
-    ) -> core::result::Result<usize, (Error, usize)> {
+    pub fn read_fallible(&mut self, writer: &mut dyn IoVecWrite) -> Result<usize> {
         let rb = &self.rb;
         let len = rb.len();
         if len == 0 {
             return Ok(0);
         }
-        let read_len = writer.avail().min(len);
+        let read_len = writer.len().min(len);
 
         let head = rb.head();
         let read_len = if head + read_len > rb.capacity {
             // Read from two separate parts
             let mut reader = rb.segment.reader().skip(head).limit(rb.capacity - head);
-            let mut len = reader.read_fallible(writer).map_err(|(e, l1)| {
-                rb.advance_head(head, l1);
-                (e.into(), l1)
-            })?;
+            let mut len = writer.write_all(&mut reader)?;
 
             let mut reader = rb.segment.reader().limit(read_len - (rb.capacity - head));
-            len += reader.read_fallible(writer).map_err(|(e, l2)| {
-                rb.advance_head(head, len + l2);
-                (e.into(), len + l2)
-            })?;
+            len += writer.write_all(&mut reader)?;
             len
         } else {
             let mut reader = rb.segment.reader().skip(head).limit(read_len);
-            reader.read_fallible(writer).map_err(|(e, len)| {
-                rb.advance_head(head, len);
-                (e.into(), len)
-            })?
+            writer.write_all(&mut reader)?
         };
 
         rb.advance_head(head, read_len);

zzj-5341/asterinas

kernel/src/net/socket/ip/datagram/bound.rs
@@ -11,6 +11,7 @@ use crate::{
     net::{iface::AnyBoundSocket, socket::util::send_recv_flags::SendRecvFlags},
     prelude::*,
     process::signal::Pollee,
+    util::{MultiRead, MultiWrite},
 };
 
 pub struct BoundDatagram {
@@ -38,43 +39,63 @@ impl BoundDatagram {
         self.remote_endpoint = Some(*endpoint)
     }
 
-    pub fn try_recv(&self, buf: &mut [u8], _flags: SendRecvFlags) -> Result<(usize, IpEndpoint)> {
-        let result = self
-            .bound_socket
-            .raw_with(|socket: &mut RawUdpSocket| socket.recv_slice(buf));
+    pub fn try_recv(
+        &self,
+        writer: &mut dyn MultiWrite,
+        _flags: SendRecvFlags,
+    ) -> Result<(usize, IpEndpoint)> {
+        let result = self.bound_socket.raw_with(|socket: &mut RawUdpSocket| {
+            socket.recv().map(|(packet, udp_metadata)| {
+                let copied_res = writer.write(&mut VmReader::from(packet));
+                let endpoint = udp_metadata.endpoint;
+                (copied_res, endpoint)
+            })
+        });
+
         match result {
-            Ok((recv_len, udp_metadata)) => Ok((recv_len, udp_metadata.endpoint)),
+            Ok((Ok(res), endpoint)) => Ok((res, endpoint)),
+            Ok((Err(e), _)) => Err(e),
             Err(RecvError::Exhausted) => {
                 return_errno_with_message!(Errno::EAGAIN, "the receive buffer is empty")
             }
             Err(RecvError::Truncated) => {
-                todo!();
+                unreachable!("`Socket::recv` should never fail with `RecvError::Truncated`")
             }
         }
     }
 
     pub fn try_send(
         &self,
-        buf: &[u8],
+        reader: &mut dyn MultiRead,
         remote: &IpEndpoint,
         _flags: SendRecvFlags,
     ) -> Result<usize> {
-        let result = self.bound_socket.raw_with(|socket: &mut RawUdpSocket| {
-            if socket.payload_send_capacity() < buf.len() {
-                return None;
+        let reader_len = reader.sum_lens();
+
+        self.bound_socket.raw_with(|socket: &mut RawUdpSocket| {
+            if socket.payload_send_capacity() < reader_len {
+                return_errno_with_message!(Errno::EMSGSIZE, "the message is too large");
             }
-            Some(socket.send_slice(buf, *remote))
-        });
-        match result {
-            Some(Ok(())) => Ok(buf.len()),
-            Some(Err(SendError::BufferFull)) => {
-                return_errno_with_message!(Errno::EAGAIN, "the send buffer is full")
-            }
-            Some(Err(SendError::Unaddressable)) => {
-                return_errno_with_message!(Errno::EINVAL, "the destination address is invalid")
-            }
-            None => return_errno_with_message!(Errno::EMSGSIZE, "the message is too large"),
-        }
+
+            let socket_buffer = match socket.send(reader_len, *remote) {
+                Ok(socket_buffer) => socket_buffer,
+                Err(SendError::BufferFull) => {
+                    return_errno_with_message!(Errno::EAGAIN, "the send buffer is full")
+                }
+                Err(SendError::Unaddressable) => {
+                    return_errno_with_message!(Errno::EINVAL, "the destination address is invalid")
+                }
+            };
+
+            // FIXME: If copy failed, we should not send any packet.
+            // But current smoltcp API seems not to support this behavior.
+            reader
+                .read(&mut VmWriter::from(socket_buffer))
+                .map_err(|e| {
+                    warn!("unexpected UDP packet will be sent");
+                    e
+                })
+        })
     }
 
     pub(super) fn init_pollee(&self, pollee: &Pollee) {

kernel/src/net/socket/vsock/common.rs
@@ -16,7 +16,7 @@ use super::{
         listen::Listen,
     },
 };
-use crate::{events::IoEvents, prelude::*, return_errno_with_message};
+use crate::{events::IoEvents, prelude::*, return_errno_with_message, util::MultiRead};
 
 /// Manage all active sockets
 pub struct VsockSpace {
@@ -190,10 +190,15 @@ impl VsockSpace {
     }
 
     /// Send a data packet
-    pub fn send(&self, buffer: &[u8], info: &mut ConnectionInfo) -> Result<()> {
+    pub fn send(&self, reader: &mut dyn MultiRead, info: &mut ConnectionInfo) -> Result<()> {
+        // FIXME: Creating this buffer should be avoided
+        // if the underlying driver can accept reader.
+        let mut buffer = vec![0u8; reader.sum_lens()];
+        reader.read(&mut VmWriter::from(buffer.as_mut_slice()))?;
+
         let mut driver = self.driver.disable_irq().lock();
         driver
-            .send(buffer, info)
+            .send(&buffer, info)
             .map_err(|_| Error::with_message(Errno::EIO, "cannot send data packet"))
     }
 

kernel/src/net/socket/vsock/stream/socket.rs
@@ -9,13 +9,12 @@ use crate::{
     events::IoEvents,
     fs::{file_handle::FileLike, utils::StatusFlags},
     net::socket::{
-        util::{copy_message_from_user, copy_message_to_user, create_message_buffer},
         vsock::{addr::VsockSocketAddr, VSOCK_GLOBAL},
         MessageHeader, SendRecvFlags, SockShutdownCmd, Socket, SocketAddr,
     },
     prelude::*,
     process::signal::{Pollable, Poller},
-    util::IoVec,
+    util::{MultiRead, MultiWrite},
 };
 
 pub struct VsockStreamSocket {
@@ -81,17 +80,21 @@ impl VsockStreamSocket {
         Ok((socket, peer_addr.into()))
     }
 
-    fn send(&self, buf: &[u8], flags: SendRecvFlags) -> Result<usize> {
+    fn send(&self, reader: &mut dyn MultiRead, flags: SendRecvFlags) -> Result<usize> {
         let inner = self.status.read();
         match &*inner {
-            Status::Connected(connected) => connected.send(buf, flags),
+            Status::Connected(connected) => connected.send(reader, flags),
             Status::Init(_) | Status::Listen(_) => {
                 return_errno_with_message!(Errno::EINVAL, "the socket is not connected");
             }
         }
     }
 
-    fn try_recv(&self, buf: &mut [u8], _flags: SendRecvFlags) -> Result<(usize, SocketAddr)> {
+    fn try_recv(
+        &self,
+        writer: &mut dyn MultiWrite,
+        _flags: SendRecvFlags,
+    ) -> Result<(usize, SocketAddr)> {
         let connected = match &*self.status.read() {
             Status::Connected(connected) => connected.clone(),
             Status::Init(_) | Status::Listen(_) => {
@@ -99,7 +102,7 @@ impl VsockStreamSocket {
             }
         };
 
-        let read_size = connected.try_recv(buf)?;
+        let read_size = connected.try_recv(writer)?;
         connected.update_io_events();
 
         let peer_addr = self.peer_addr()?;
@@ -113,11 +116,15 @@ impl VsockStreamSocket {
         Ok((read_size, peer_addr))
     }
 
-    fn recv(&self, buf: &mut [u8], flags: SendRecvFlags) -> Result<(usize, SocketAddr)> {
+    fn recv(
+        &self,
+        writer: &mut dyn MultiWrite,
+        flags: SendRecvFlags,
+    ) -> Result<(usize, SocketAddr)> {
         if self.is_nonblocking() {
-            self.try_recv(buf, flags)
+            self.try_recv(writer, flags)
         } else {
-            self.wait_events(IoEvents::IN, || self.try_recv(buf, flags))
+            self.wait_events(IoEvents::IN, || self.try_recv(writer, flags))
         }
     }
 }
@@ -138,19 +145,16 @@ impl FileLike for VsockStreamSocket {
     }
 
     fn read(&self, writer: &mut VmWriter) -> Result<usize> {
-        let mut buf = vec![0u8; writer.avail()];
         // TODO: Set correct flags
         let read_len = self
-            .recv(&mut buf, SendRecvFlags::empty())
+            .recv(writer, SendRecvFlags::empty())
             .map(|(len, _)| len)?;
-        writer.write_fallible(&mut buf.as_slice().into())?;
         Ok(read_len)
     }
 
     fn write(&self, reader: &mut VmReader) -> Result<usize> {
-        let buf = reader.collect()?;
         // TODO: Set correct flags
-        self.send(&buf, SendRecvFlags::empty())
+        self.send(reader, SendRecvFlags::empty())
     }
 
     fn status_flags(&self) -> StatusFlags {
@@ -285,7 +289,7 @@ impl Socket for VsockStreamSocket {
 
     fn sendmsg(
         &self,
-        io_vecs: &[IoVec],
+        reader: &mut dyn MultiRead,
         message_header: MessageHeader,
         flags: SendRecvFlags,
     ) -> Result<usize> {
@@ -301,28 +305,24 @@ impl Socket for VsockStreamSocket {
             warn!("sending control message is not supported");
         }
 
-        let buf = copy_message_from_user(io_vecs);
-        self.send(&buf, flags)
+        self.send(reader, flags)
     }
 
-    fn recvmsg(&self, io_vecs: &[IoVec], flags: SendRecvFlags) -> Result<(usize, MessageHeader)> {
+    fn recvmsg(
+        &self,
+        writer: &mut dyn MultiWrite,
+        flags: SendRecvFlags,
+    ) -> Result<(usize, MessageHeader)> {
         // TODO: Deal with flags
         debug_assert!(flags.is_all_supported());
 
-        let mut buf = create_message_buffer(io_vecs);
-
-        let (received_bytes, _) = self.recv(&mut buf, flags)?;
-
-        let copied_bytes = {
-            let message = &buf[..received_bytes];
-            copy_message_to_user(io_vecs, message)
-        };
+        let (received_bytes, _) = self.recv(writer, flags)?;
 
         // TODO: Receive control message
 
         let messsge_header = MessageHeader::new(None, None);
 
-        Ok((copied_bytes, messsge_header))
+        Ok((received_bytes, messsge_header))
     }
 
     fn addr(&self) -> Result<SocketAddr> {

kernel/src/util/ring_buffer.rs
@@ -10,6 +10,7 @@ use align_ext::AlignExt;
 use inherit_methods_macro::inherit_methods;
 use ostd::mm::{FrameAllocOptions, Segment, VmIo};
 
+use super::{MultiRead, MultiWrite};
 use crate::prelude::*;
 
 /// A lock-free SPSC FIFO ring buffer backed by a [`Segment`].
@@ -220,13 +221,10 @@ impl<T: Pod> RingBuffer<T> {
 }
 
 impl RingBuffer<u8> {
-    /// Writes data from the `VmReader` to the `RingBuffer`.
+    /// Writes data from the `reader` to the `RingBuffer`.
     ///
     /// Returns the number of bytes written.
-    pub fn write_fallible(
-        &mut self,
-        reader: &mut VmReader,
-    ) -> core::result::Result<usize, (Error, usize)> {
+    pub fn write_fallible(&mut self, reader: &mut dyn MultiRead) -> Result<usize> {
         let mut producer = Producer {
             rb: self,
             phantom: PhantomData,
@@ -234,13 +232,10 @@ impl RingBuffer<u8> {
         producer.write_fallible(reader)
     }
 
-    /// Reads data from the `VmWriter` to the `RingBuffer`.
+    /// Reads data from the `writer` to the `RingBuffer`.
     ///
     /// Returns the number of bytes read.
-    pub fn read_fallible(
-        &mut self,
-        writer: &mut VmWriter,
-    ) -> core::result::Result<usize, (Error, usize)> {
+    pub fn read_fallible(&mut self, writer: &mut dyn MultiWrite) -> Result<usize> {
         let mut consumer = Consumer {
             rb: self,
             phantom: PhantomData,
@@ -310,38 +305,26 @@ impl<R: Deref<Target = RingBuffer<u8>>> Producer<u8, R> {
     /// Writes data from the `VmReader` to the `RingBuffer`.
     ///
     /// Returns the number of bytes written.
-    pub fn write_fallible(
-        &mut self,
-        reader: &mut VmReader,
-    ) -> core::result::Result<usize, (Error, usize)> {
+    pub fn write_fallible(&mut self, reader: &mut dyn MultiRead) -> Result<usize> {
         let rb = &self.rb;
         let free_len = rb.free_len();
         if free_len == 0 {
             return Ok(0);
         }
-        let write_len = reader.remain().min(free_len);
+        let write_len = reader.sum_lens().min(free_len);
 
         let tail = rb.tail();
         let write_len = if tail + write_len > rb.capacity {
             // Write into two separate parts
             let mut writer = rb.segment.writer().skip(tail).limit(rb.capacity - tail);
-            let mut len = writer.write_fallible(reader).map_err(|(e, l1)| {
-                rb.advance_tail(tail, l1);
-                (e.into(), l1)
-            })?;
+            let mut len = reader.read(&mut writer)?;
 
             let mut writer = rb.segment.writer().limit(write_len - (rb.capacity - tail));
-            len += writer.write_fallible(reader).map_err(|(e, l2)| {
-                rb.advance_tail(tail, len + l2);
-                (e.into(), len + l2)
-            })?;
+            len += reader.read(&mut writer)?;
             len
         } else {
             let mut writer = rb.segment.writer().skip(tail).limit(write_len);
-            writer.write_fallible(reader).map_err(|(e, len)| {
-                rb.advance_tail(tail, len);
-                (e.into(), len)
-            })?
+            reader.read(&mut writer)?
         };
 
         rb.advance_tail(tail, write_len);
@@ -418,38 +401,26 @@ impl<R: Deref<Target = RingBuffer<u8>>> Consumer<u8, R> {
     /// Reads data from the `VmWriter` to the `RingBuffer`.
     ///
     /// Returns the number of bytes read.
-    pub fn read_fallible(
-        &mut self,
-        writer: &mut VmWriter,
-    ) -> core::result::Result<usize, (Error, usize)> {
+    pub fn read_fallible(&mut self, writer: &mut dyn MultiWrite) -> Result<usize> {
         let rb = &self.rb;
         let len = rb.len();
         if len == 0 {
             return Ok(0);
         }
-        let read_len = writer.avail().min(len);
+        let read_len = writer.sum_lens().min(len);
 
         let head = rb.head();
         let read_len = if head + read_len > rb.capacity {
             // Read from two separate parts
             let mut reader = rb.segment.reader().skip(head).limit(rb.capacity - head);
-            let mut len = reader.read_fallible(writer).map_err(|(e, l1)| {
-                rb.advance_head(head, l1);
-                (e.into(), l1)
-            })?;
+            let mut len = writer.write(&mut reader)?;
 
             let mut reader = rb.segment.reader().limit(read_len - (rb.capacity - head));
-            len += reader.read_fallible(writer).map_err(|(e, l2)| {
-                rb.advance_head(head, len + l2);
-                (e.into(), len + l2)
-            })?;
+            len += writer.write(&mut reader)?;
             len
         } else {
             let mut reader = rb.segment.reader().skip(head).limit(read_len);
-            reader.read_fallible(writer).map_err(|(e, len)| {
-                rb.advance_head(head, len);
-                (e.into(), len)
-            })?
+            writer.write(&mut reader)?
         };
 
         rb.advance_head(head, read_len);