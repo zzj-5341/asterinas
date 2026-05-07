8903871d753156e94189e0134360e3459824c4a2
zzj-5341 <zzj1104387484@gmail.com>
2026-05-07 12:59:37 +0800

    Rename and simplify Yama regression test

--- test/initramfs/src/regression/security/lsm/yama.c
+++ test/initramfs/src/regression/security/lsm/yama.c
@@ -4,12 +4,12 @@
 
 #include <errno.h>
 #include <fcntl.h>
+#include <linux/capability.h>
 #include <signal.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <linux/capability.h>
 #include <sys/syscall.h>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -50,19 +50,9 @@ static int pidfd_getfd_syscall(int pidfd, int targetfd)
 static int read_scope(void)
 {
 	char buf[16] = { 0 };
-
-	int fd = open(YAMA_PTRACE_SCOPE, O_RDONLY);
-	if (fd < 0) {
-		return -1;
-	}
-
-	ssize_t len = read(fd, buf, sizeof(buf) - 1);
-	int saved_errno = errno;
-	close(fd);
-	errno = saved_errno;
-	if (len < 0) {
-		return -1;
-	}
+	int fd = CHECK(open(YAMA_PTRACE_SCOPE, O_RDONLY));
+	ssize_t len = CHECK(read(fd, buf, sizeof(buf) - 1));
+	CHECK(close(fd));
 
 	buf[len] = '\0';
 	return atoi(buf);
@@ -71,26 +61,10 @@ static int read_scope(void)
 static int write_scope(int scope)
 {
 	char buf[16];
-
-	int fd = open(YAMA_PTRACE_SCOPE, O_WRONLY);
-	if (fd < 0) {
-		return -1;
-	}
-
-	int len = snprintf(buf, sizeof(buf), "%d\n", scope);
-	ssize_t written = write(fd, buf, len);
-	int saved_errno = errno;
-	close(fd);
-	errno = saved_errno;
-
-	if (written != len) {
-		if (written >= 0) {
-			errno = EIO;
-		}
-		return -1;
-	}
-
-	return 0;
+	int fd = CHECK(open(YAMA_PTRACE_SCOPE, O_WRONLY));
+	int len = CHECK(snprintf(buf, sizeof(buf), "%d\n", scope));
+	CHECK_WITH(write(fd, buf, len), _ret == len);
+	return close(fd);
 }
 
 static void drop_cap_sys_ptrace(void)
@@ -116,13 +90,12 @@ static pid_t spawn_target_process(bool drop_ptrace_cap)
 
 	pid_t target_pid = CHECK(fork());
 	if (target_pid == 0) {
-		close(ready_pipe[0]);
+		CHECK(close(ready_pipe[0]));
 		if (drop_ptrace_cap) {
 			drop_cap_sys_ptrace();
 		}
 
-		int fd =
-			CHECK(open(TESTFILE, O_CREAT | O_RDWR | O_TRUNC, 0644));
+		int fd = CHECK(open(TESTFILE, O_CREAT | O_RDWR | O_TRUNC, 0644));
 		CHECK_WITH(write(fd, TEST_CONTENT, strlen(TEST_CONTENT)),
 			   _ret == (ssize_t)strlen(TEST_CONTENT));
 		CHECK(dup2(fd, TARGET_FD));
@@ -134,88 +107,19 @@ static pid_t spawn_target_process(bool drop_ptrace_cap)
 		_exit(EXIT_SUCCESS);
 	}
 
-	close(ready_pipe[1]);
+	CHECK(close(ready_pipe[1]));
 	char ready = '\0';
 	CHECK_WITH(read(ready_pipe[0], &ready, 1), _ret == 1);
 	CHECK(close(ready_pipe[0]));
-
 	return target_pid;
-}
-
-static int run_sibling_pidfd_getfd(pid_t target_pid, bool expect_success,
-				   bool drop_ptrace_cap)
-{
-	pid_t attacker_pid = fork();
-	if (attacker_pid < 0) {
-		return -1;
-	}
-
-	if (attacker_pid == 0) {
-		if (drop_ptrace_cap) {
-			drop_cap_sys_ptrace();
-		}
-
-		int pidfd = pidfd_open_syscall(target_pid);
-		if (pidfd < 0) {
-			_exit(10);
-		}
-
-		int duplicated_fd = pidfd_getfd_syscall(pidfd, TARGET_FD);
-		if (!expect_success) {
-			int saved_errno = errno;
-			close(pidfd);
-			if (duplicated_fd >= 0) {
-				close(duplicated_fd);
-				_exit(11);
-			}
-			if (saved_errno != EPERM) {
-				_exit(12);
-			}
-			_exit(EXIT_SUCCESS);
-		}
-
-		if (duplicated_fd < 0) {
-			close(pidfd);
-			_exit(20);
-		}
-
-		char buf[32] = { 0 };
-		ssize_t len = pread(duplicated_fd, buf, sizeof(buf) - 1, 0);
-		close(duplicated_fd);
-		close(pidfd);
-		if (len < 0) {
-			_exit(21);
-		}
-
-		buf[len] = '\0';
-		if (strcmp(buf, TEST_CONTENT) != 0) {
-			_exit(22);
-		}
-
-		_exit(EXIT_SUCCESS);
-	}
-
-	int status = 0;
-	if (waitpid(attacker_pid, &status, 0) < 0) {
-		return -1;
-	}
-
-	if (!WIFEXITED(status) || WEXITSTATUS(status) != 0) {
-		errno = ECHILD;
-		return -1;
-	}
-
-	return 0;
 }
 
 static void cleanup_target_process(pid_t target_pid)
 {
-	if (target_pid <= 0) {
-		return;
+	if (target_pid > 0) {
+		kill(target_pid, SIGKILL);
+		waitpid(target_pid, NULL, 0);
 	}
-
-	kill(target_pid, SIGKILL);
-	waitpid(target_pid, NULL, 0);
 	unlink(TESTFILE);
 }
 
@@ -225,7 +129,7 @@ FN_SETUP(save_initial_scope)
 }
 END_SETUP()
 
-FN_TEST(yama_procfs_roundtrip)
+FN_TEST(procfs_roundtrip)
 {
 	TEST_SUCC(write_scope(YAMA_SCOPE_DISABLED));
 	TEST_RES(read_scope(), _ret == YAMA_SCOPE_DISABLED);
@@ -235,24 +139,81 @@ FN_TEST(yama_procfs_roundtrip)
 }
 END_TEST()
 
-FN_TEST(yama_relational_denies_sibling_pidfd_getfd)
+FN_TEST(relational_denies_sibling_pidfd_getfd)
 {
 	TEST_SUCC(write_scope(YAMA_SCOPE_RELATIONAL));
-	pid_t target_pid = CHECK(spawn_target_process(true));
+	pid_t target_pid = TEST_SUCC(spawn_target_process(true));
+	pid_t attacker_pid = TEST_SUCC(fork());
 
-	TEST_SUCC(run_sibling_pidfd_getfd(target_pid, false, true));
+	if (attacker_pid == 0) {
+		drop_cap_sys_ptrace();
+
+		int pidfd = pidfd_open_syscall(target_pid);
+		if (pidfd < 0) {
+			_exit(10);
+		}
+
+		int duplicated_fd = pidfd_getfd_syscall(pidfd, TARGET_FD);
+		int saved_errno = errno;
+		close(pidfd);
+		if (duplicated_fd >= 0) {
+			close(duplicated_fd);
+			_exit(11);
+		}
+		if (saved_errno != EPERM) {
+			_exit(12);
+		}
+		_exit(EXIT_SUCCESS);
+	}
+
+	int status = 0;
+	TEST_RES(waitpid(attacker_pid, &status, 0),
+		 WIFEXITED(status) && WEXITSTATUS(status) == 0);
 
 	cleanup_target_process(target_pid);
 	TEST_SUCC(write_scope(saved_scope));
 }
 END_TEST()
 
-FN_TEST(yama_disabled_allows_sibling_pidfd_getfd)
+FN_TEST(disabled_allows_sibling_pidfd_getfd)
 {
 	TEST_SUCC(write_scope(YAMA_SCOPE_DISABLED));
-	pid_t target_pid = CHECK(spawn_target_process(true));
+	pid_t target_pid = TEST_SUCC(spawn_target_process(true));
+	pid_t attacker_pid = TEST_SUCC(fork());
 
-	TEST_SUCC(run_sibling_pidfd_getfd(target_pid, true, true));
+	if (attacker_pid == 0) {
+		drop_cap_sys_ptrace();
+
+		int pidfd = pidfd_open_syscall(target_pid);
+		if (pidfd < 0) {
+			_exit(20);
+		}
+
+		int duplicated_fd = pidfd_getfd_syscall(pidfd, TARGET_FD);
+		if (duplicated_fd < 0) {
+			close(pidfd);
+			_exit(21);
+		}
+
+		char buf[32] = { 0 };
+		ssize_t len = pread(duplicated_fd, buf, sizeof(buf) - 1, 0);
+		close(duplicated_fd);
+		close(pidfd);
+		if (len < 0) {
+			_exit(22);
+		}
+
+		buf[len] = '\0';
+		if (strcmp(buf, TEST_CONTENT) != 0) {
+			_exit(23);
+		}
+
+		_exit(EXIT_SUCCESS);
+	}
+
+	int status = 0;
+	TEST_RES(waitpid(attacker_pid, &status, 0),
+		 WIFEXITED(status) && WEXITSTATUS(status) == 0);
 
 	cleanup_target_process(target_pid);
 	TEST_SUCC(write_scope(saved_scope));
