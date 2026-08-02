eeb8dbd0608c1c714ebc979e43ef8155eacf053e
zzj-5341 <242576472+zzj-5341@users.noreply.github.com>
2026-08-02 20:12:34 +0800

    Enforce AppArmor checks for open variants

--- kernel/src/syscall/open.rs
+++ kernel/src/syscall/open.rs
@@ -69,43 +69,7 @@ pub fn sys_openat(
 }
 
 pub fn sys_open(path_addr: Vaddr, flags: u32, mode: u16, ctx: &Context) -> Result<SyscallReturn> {
-    let path = ctx.user_space().read_cstring(path_addr, MAX_FILENAME_LEN)?;
-    debug!("path = {:?}, flags = {}, mode = {}", path, flags, mode);
-
-    let file_handle = {
-        let path = path.to_string_lossy();
-        let fs_path = FsPath::from_fd_at(AT_FDCWD, path.as_ref(), EmptyPathStr::Reject)?;
-
-        let fs_ref = ctx.thread_local.borrow_fs();
-        let mask_mode = mode & !fs_ref.umask().get();
-
-        let path_resolver = fs_ref.resolver().read();
-        do_open_with_file_open_check(
-            &path_resolver,
-            &fs_path,
-            flags,
-            InodeMode::from_bits_truncate(mask_mode),
-        )
-        .map_err(|err| match err.error() {
-            Errno::EINTR => Error::new(Errno::ERESTARTSYS),
-            _ => err,
-        })?
-    };
-
-    let fd = {
-        let file_table = ctx.thread_local.borrow_file_table();
-        let mut file_table_locked = file_table.unwrap().write();
-        let fd_flags =
-            if CreationFlags::from_bits_truncate(flags).contains(CreationFlags::O_CLOEXEC) {
-                FdFlags::CLOEXEC
-            } else {
-                FdFlags::empty()
-            };
-        file_table_locked.insert(file_handle.clone(), fd_flags)
-    };
-    let file_like: Arc<dyn FileLike> = file_handle;
-    fs::vfs::notify::on_open(&file_like);
-    Ok(SyscallReturn::Return(fd.into()))
+    sys_openat(AT_FDCWD, path_addr, flags, mode, ctx)
 }
 
 pub fn sys_creat(path_addr: Vaddr, mode: u16, ctx: &Context) -> Result<SyscallReturn> {
@@ -124,56 +88,6 @@ fn do_open(
 
     if open_args.is_tmpfile() {
         return do_open_tmpfile(path_resolver, fs_path, &open_args);
-    }
-
-    let lookup_res = if open_args.follow_tail_link() {
-        path_resolver.lookup_unresolved(fs_path)?
-    } else {
-        path_resolver.lookup_unresolved_no_follow(fs_path)?
-    };
-
-    let file_handle: Arc<dyn FileLike> = match lookup_res {
-        LookupResult::Resolved(path) => Arc::new(path.open(open_args)?),
-        LookupResult::AtParent(result) => {
-            if !open_args.creation_flags.contains(CreationFlags::O_CREAT)
-                || open_args.status_flags.contains(StatusFlags::O_PATH)
-            {
-                return_errno_with_message!(Errno::ENOENT, "the file does not exist");
-            }
-            if result.target_is_dir() {
-                return_errno_with_message!(
-                    Errno::EISDIR,
-                    "O_CREAT is specified but the file is a directory"
-                );
-            }
-
-            let (parent, tail_name) = result.into_parent_and_basename();
-            let new_path =
-                parent.new_fs_child(&tail_name, InodeType::File, open_args.inode_mode)?;
-            fs::vfs::notify::on_create(&parent, || tail_name.clone());
-
-            // Don't check access mode for newly created file.
-            Arc::new(InodeHandle::new_unchecked_access(
-                new_path,
-                open_args.access_mode,
-                open_args.status_flags,
-            )?)
-        }
-    };
-
-    Ok(file_handle)
-}
-
-fn do_open_with_file_open_check(
-    path_resolver: &PathResolver,
-    fs_path: &FsPath,
-    flags: u32,
-    mode: InodeMode,
-) -> Result<Arc<dyn FileLike>> {
-    let open_args = OpenArgs::from_flags_and_mode(flags, mode)?;
-
-    if open_args.is_tmpfile() {
-        return do_open_tmpfile_with_file_open_check(path_resolver, fs_path, &open_args);
     }
 
     let lookup_res = if open_args.follow_tail_link() {
@@ -229,7 +143,7 @@ fn do_open_with_file_open_check(
     Ok(file_handle)
 }
 
-fn do_open_tmpfile_with_file_open_check(
+fn do_open_tmpfile(
     path_resolver: &PathResolver,
     fs_path: &FsPath,
     open_args: &OpenArgs,
@@ -257,33 +171,6 @@ fn do_open_tmpfile_with_file_open_check(
         open_args.creation_flags,
         open_args.status_flags,
     )?;
-    let tmpfile_path = dir_path.create_tmpfile(open_args.inode_mode, hard_linkability)?;
-    Ok(Arc::new(InodeHandle::new_unchecked_access(
-        tmpfile_path,
-        open_args.access_mode,
-        open_args.status_flags,
-    )?))
-}
-
-fn do_open_tmpfile(
-    path_resolver: &PathResolver,
-    fs_path: &FsPath,
-    open_args: &OpenArgs,
-) -> Result<Arc<dyn FileLike>> {
-    let dir_path = if open_args.follow_tail_link() {
-        path_resolver.lookup(fs_path)?
-    } else {
-        path_resolver.lookup_no_follow(fs_path)?
-    };
-
-    // `O_EXCL` with `O_TMPFILE` is allowed by Linux, but it prevents the tmpfile
-    // from being linked later by `linkat(..., AT_EMPTY_PATH)`.
-    // Reference: <https://man7.org/linux/man-pages/man2/open.2.html>.
-    let hard_linkability = if open_args.creation_flags.contains(CreationFlags::O_EXCL) {
-        HardLinkability::Unlinkable
-    } else {
-        HardLinkability::Linkable
-    };
     let tmpfile_path = dir_path.create_tmpfile(open_args.inode_mode, hard_linkability)?;
     Ok(Arc::new(InodeHandle::new_unchecked_access(
         tmpfile_path,
--- test/initramfs/src/regression/security/lsm/apparmor.c
+++ test/initramfs/src/regression/security/lsm/apparmor.c
@@ -30,6 +30,12 @@ static const char *DENIED_EXISTING_PATH = "/tmp/apparmor-denied-existing";
 static const char *CREATE_ALLOWED_PATH = "/tmp/apparmor-create-allowed";
 static const char *CREATE_READ_ONLY_PATH = "/tmp/apparmor-create-read-only";
 static const char *CREATE_DENIED_PATH = "/tmp/apparmor-create-denied";
+static const char *OPENAT_CREATE_ALLOWED_PATH =
+	"/tmp/apparmor-openat-create-allowed";
+static const char *OPENAT_CREATE_DENIED_PATH =
+	"/tmp/apparmor-openat-create-denied";
+static const char *CREAT_ALLOWED_PATH = "/tmp/apparmor-creat-allowed";
+static const char *CREAT_DENIED_PATH = "/tmp/apparmor-creat-denied";
 static const char *READ_ONLY_DIR = "/tmp/apparmor-read-only-mount";
 static const char *READ_ONLY_CREATE_PATH =
 	"/tmp/apparmor-read-only-mount/create";
@@ -41,7 +47,6 @@ static const char *RESERVED_NAME_POLICY =
 static const char *UNPRIVILEGED_POLICY =
 	"profile apparmor-unprivileged-load\n/tmp/apparmor-unused r\n";
 
-static bool apparmor_enabled;
 static bool securityfs_mounted_by_test;
 static char current_attr_path[CURRENT_ATTR_PATH_BUFFER_SIZE];
 
@@ -58,6 +63,44 @@ static int invoke_open_syscall(const char *path, int flags, mode_t mode)
 {
 #ifdef SYS_open
 	return (int)syscall(SYS_open, path, flags, mode);
+#else
+	errno = ENOSYS;
+	return -1;
+#endif
+}
+
+static bool openat_syscall_supported(void)
+{
+#ifdef SYS_openat
+	return true;
+#else
+	return false;
+#endif
+}
+
+static int invoke_openat_syscall(const char *path, int flags, mode_t mode)
+{
+#ifdef SYS_openat
+	return (int)syscall(SYS_openat, AT_FDCWD, path, flags, mode);
+#else
+	errno = ENOSYS;
+	return -1;
+#endif
+}
+
+static bool creat_syscall_supported(void)
+{
+#ifdef SYS_creat
+	return true;
+#else
+	return false;
+#endif
+}
+
+static int invoke_creat_syscall(const char *path, mode_t mode)
+{
+#ifdef SYS_creat
+	return (int)syscall(SYS_creat, path, mode);
 #else
 	errno = ENOSYS;
 	return -1;
@@ -88,10 +131,14 @@ static void load_profile(void)
 				    "%s w\n"
 				    "%s rw\n"
 				    "%s r\n"
+				    "%s rw\n"
+				    "%s w\n"
 				    "%s r\n",
 				    PROFILE_NAME, READ_ONLY_PATH,
 				    WRITE_ONLY_PATH, CREATE_ALLOWED_PATH,
-				    CREATE_READ_ONLY_PATH, current_attr_path),
+				    CREATE_READ_ONLY_PATH,
+				    OPENAT_CREATE_ALLOWED_PATH,
+				    CREAT_ALLOWED_PATH, current_attr_path),
 			   _ret >= 0 && _ret < (int)sizeof(policy));
 
 	fd = CHECK(open(APPARMOR_LOAD_PATH, O_WRONLY));
@@ -139,12 +186,7 @@ FN_SETUP(prepare_apparmor_environment)
 	securityfs_mounted_by_test = mount_result == 0;
 
 	struct stat statbuf;
-	int stat_result = CHECK_WITH(stat(APPARMOR_LOAD_PATH, &statbuf),
-				     _ret == 0 || errno == ENOENT);
-	apparmor_enabled = stat_result == 0;
-	if (!apparmor_enabled) {
-		return;
-	}
+	CHECK(stat(APPARMOR_LOAD_PATH, &statbuf));
 
 	CHECK_WITH(snprintf(current_attr_path, sizeof(current_attr_path),
 			    "/proc/%ld/attr/current", (long)getpid()),
@@ -155,6 +197,12 @@ FN_SETUP(prepare_apparmor_environment)
 	CHECK_WITH(unlink(CREATE_ALLOWED_PATH), _ret == 0 || errno == ENOENT);
 	CHECK_WITH(unlink(CREATE_READ_ONLY_PATH), _ret == 0 || errno == ENOENT);
 	CHECK_WITH(unlink(CREATE_DENIED_PATH), _ret == 0 || errno == ENOENT);
+	CHECK_WITH(unlink(OPENAT_CREATE_ALLOWED_PATH),
+		   _ret == 0 || errno == ENOENT);
+	CHECK_WITH(unlink(OPENAT_CREATE_DENIED_PATH),
+		   _ret == 0 || errno == ENOENT);
+	CHECK_WITH(unlink(CREAT_ALLOWED_PATH), _ret == 0 || errno == ENOENT);
+	CHECK_WITH(unlink(CREAT_DENIED_PATH), _ret == 0 || errno == ENOENT);
 	CHECK_WITH(mkdir(READ_ONLY_DIR, 0700), _ret == 0 || errno == EEXIST);
 	CHECK(mount("ramfs", READ_ONLY_DIR, "ramfs", MS_RDONLY, NULL));
 }
@@ -163,8 +211,6 @@ END_SETUP()
 FN_TEST(policy_load_requires_mac_admin)
 {
 	pid_t child;
-
-	SKIP_TEST_IF(!apparmor_enabled);
 
 	child = TEST_SUCC(fork());
 	if (child == 0) {
@@ -198,8 +244,6 @@ FN_TEST(policy_load_rejects_non_absolute_paths)
 {
 	int fd;
 
-	SKIP_TEST_IF(!apparmor_enabled);
-
 	fd = TEST_SUCC(open(APPARMOR_LOAD_PATH, O_WRONLY));
 	if (fd >= 0) {
 		TEST_ERRNO(write(fd, INVALID_POLICY, strlen(INVALID_POLICY)),
@@ -212,8 +256,6 @@ END_TEST()
 FN_TEST(policy_load_rejects_reserved_profile_name)
 {
 	int fd;
-
-	SKIP_TEST_IF(!apparmor_enabled);
 
 	fd = TEST_SUCC(open(APPARMOR_LOAD_PATH, O_WRONLY));
 	if (fd >= 0) {
@@ -231,10 +273,6 @@ END_TEST()
  */
 FN_SETUP(load_and_attach_apparmor_profile)
 {
-	if (!apparmor_enabled) {
-		return;
-	}
-
 	load_profile();
 	attach_profile();
 }
@@ -245,8 +283,6 @@ FN_TEST(task_label_is_visible)
 	char label[LABEL_BUFFER_SIZE];
 	int fd;
 	ssize_t len;
-
-	SKIP_TEST_IF(!apparmor_enabled);
 
 	fd = TEST_SUCC(open(current_attr_path, O_RDONLY));
 	len = TEST_SUCC(read(fd, label, sizeof(label) - 1));
@@ -265,7 +301,7 @@ FN_TEST(profile_controls_read_access)
 	char contents[sizeof(FILE_CONTENT) - 1];
 	int fd;
 
-	SKIP_TEST_IF(!apparmor_enabled || !open_syscall_supported());
+	SKIP_TEST_IF(!open_syscall_supported());
 
 	TEST_ERRNO(invoke_open_syscall(READ_ONLY_PATH, O_WRONLY, 0), EACCES);
 	TEST_ERRNO(invoke_open_syscall(READ_ONLY_PATH, O_RDONLY | O_TRUNC, 0),
@@ -284,7 +320,7 @@ FN_TEST(profile_allows_path_handles_without_data_access)
 {
 	int fd;
 
-	SKIP_TEST_IF(!apparmor_enabled || !open_syscall_supported());
+	SKIP_TEST_IF(!open_syscall_supported());
 
 	fd = TEST_SUCC(invoke_open_syscall(DENIED_EXISTING_PATH, O_PATH, 0));
 	if (fd >= 0) {
@@ -297,7 +333,7 @@ END_TEST()
 
 FN_TEST(profile_preserves_open_error_precedence)
 {
-	SKIP_TEST_IF(!apparmor_enabled || !open_syscall_supported());
+	SKIP_TEST_IF(!open_syscall_supported());
 
 	TEST_ERRNO(invoke_open_syscall("/tmp", O_WRONLY, 0), EISDIR);
 	TEST_ERRNO(invoke_open_syscall(DENIED_EXISTING_PATH,
@@ -317,7 +353,7 @@ FN_TEST(profile_controls_file_creation)
 	struct stat statbuf;
 	int fd;
 
-	SKIP_TEST_IF(!apparmor_enabled || !open_syscall_supported());
+	SKIP_TEST_IF(!open_syscall_supported());
 
 	fd = TEST_SUCC(invoke_open_syscall(CREATE_ALLOWED_PATH,
 					   O_RDWR | O_CREAT, 0600));
@@ -343,9 +379,69 @@ FN_TEST(profile_controls_file_creation)
 }
 END_TEST()
 
+FN_TEST(profile_controls_openat_access)
+{
+	struct stat statbuf;
+	int fd;
+
+	SKIP_TEST_IF(!openat_syscall_supported());
+
+	fd = TEST_SUCC(
+		invoke_openat_syscall(READ_ONLY_PATH, O_RDONLY, 0));
+	if (fd >= 0) {
+		TEST_SUCC(close(fd));
+	}
+	TEST_ERRNO(invoke_openat_syscall(READ_ONLY_PATH, O_WRONLY, 0),
+		   EACCES);
+	TEST_ERRNO(invoke_openat_syscall(READ_ONLY_PATH,
+					 O_RDONLY | O_TRUNC, 0),
+		   EACCES);
+	TEST_RES(stat(READ_ONLY_PATH, &statbuf),
+		 _ret == 0 &&
+			 statbuf.st_size == (off_t)(sizeof(FILE_CONTENT) - 1));
+
+	fd = TEST_SUCC(invoke_openat_syscall(OPENAT_CREATE_ALLOWED_PATH,
+					    O_RDWR | O_CREAT, 0600));
+	if (fd >= 0) {
+		TEST_SUCC(close(fd));
+	}
+	TEST_SUCC(stat(OPENAT_CREATE_ALLOWED_PATH, &statbuf));
+
+	TEST_ERRNO(invoke_openat_syscall(OPENAT_CREATE_DENIED_PATH,
+					 O_RDONLY | O_CREAT, 0600),
+		   EACCES);
+	TEST_ERRNO(stat(OPENAT_CREATE_DENIED_PATH, &statbuf), ENOENT);
+	TEST_ERRNO(invoke_openat_syscall("/tmp", O_TMPFILE | O_RDWR, 0600),
+		   EACCES);
+}
+END_TEST()
+
+FN_TEST(profile_controls_creat_access)
+{
+	struct stat statbuf;
+	int fd;
+
+	SKIP_TEST_IF(!creat_syscall_supported());
+
+	fd = TEST_SUCC(invoke_creat_syscall(CREAT_ALLOWED_PATH, 0600));
+	if (fd >= 0) {
+		TEST_SUCC(close(fd));
+	}
+	TEST_RES(stat(CREAT_ALLOWED_PATH, &statbuf),
+		 _ret == 0 && statbuf.st_size == 0);
+
+	TEST_ERRNO(invoke_creat_syscall(CREAT_DENIED_PATH, 0600), EACCES);
+	TEST_ERRNO(stat(CREAT_DENIED_PATH, &statbuf), ENOENT);
+	TEST_ERRNO(invoke_creat_syscall(READ_ONLY_PATH, 0600), EACCES);
+	TEST_RES(stat(READ_ONLY_PATH, &statbuf),
+		 _ret == 0 &&
+			 statbuf.st_size == (off_t)(sizeof(FILE_CONTENT) - 1));
+}
+END_TEST()
+
 FN_TEST(profile_denies_unnamed_temporary_file)
 {
-	SKIP_TEST_IF(!apparmor_enabled || !open_syscall_supported());
+	SKIP_TEST_IF(!open_syscall_supported());
 
 	TEST_ERRNO(invoke_open_syscall("/tmp", O_TMPFILE | O_RDWR, 0600),
 		   EACCES);
@@ -356,7 +452,7 @@ FN_TEST(profile_controls_write_access)
 {
 	int fd;
 
-	SKIP_TEST_IF(!apparmor_enabled || !open_syscall_supported());
+	SKIP_TEST_IF(!open_syscall_supported());
 
 	fd = TEST_SUCC(invoke_open_syscall(WRITE_ONLY_PATH, O_WRONLY, 0));
 	if (fd >= 0) {
@@ -371,7 +467,7 @@ FN_TEST(task_label_is_inherited)
 {
 	pid_t child;
 
-	SKIP_TEST_IF(!apparmor_enabled || !open_syscall_supported());
+	SKIP_TEST_IF(!open_syscall_supported());
 
 	child = TEST_SUCC(fork());
 	if (child == 0) {
@@ -392,15 +488,18 @@ END_TEST()
 /* Keep cleanup last because the preceding tests use these files and mount. */
 FN_SETUP(cleanup_apparmor_environment)
 {
-	if (apparmor_enabled) {
-		CHECK(unlink(READ_ONLY_PATH));
-		CHECK(unlink(WRITE_ONLY_PATH));
-		CHECK(unlink(DENIED_EXISTING_PATH));
-		CHECK_WITH(unlink(CREATE_ALLOWED_PATH),
-			   _ret == 0 || errno == ENOENT);
-		CHECK(umount(READ_ONLY_DIR));
-		CHECK(rmdir(READ_ONLY_DIR));
-	}
+	CHECK(unlink(READ_ONLY_PATH));
+	CHECK(unlink(WRITE_ONLY_PATH));
+	CHECK(unlink(DENIED_EXISTING_PATH));
+	CHECK_WITH(unlink(CREATE_ALLOWED_PATH), _ret == 0 || errno == ENOENT);
+	CHECK_WITH(unlink(OPENAT_CREATE_ALLOWED_PATH),
+		   _ret == 0 || errno == ENOENT);
+	CHECK_WITH(unlink(OPENAT_CREATE_DENIED_PATH),
+		   _ret == 0 || errno == ENOENT);
+	CHECK_WITH(unlink(CREAT_ALLOWED_PATH), _ret == 0 || errno == ENOENT);
+	CHECK_WITH(unlink(CREAT_DENIED_PATH), _ret == 0 || errno == ENOENT);
+	CHECK(umount(READ_ONLY_DIR));
+	CHECK(rmdir(READ_ONLY_DIR));
 
 	if (securityfs_mounted_by_test) {
 		CHECK(umount(SECURITYFS_DIR));
